d10fd4d9d3c4707cc4411746f08c02b4aac07c4f
JuiceTanZ <66227184+JuiceTanZ@users.noreply.github.com>
2020-06-24 17:02:47 -0700

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -5,4 +5,18 @@ const prev = document.querySelector('#prev');
 const next = document.querySelector('#next');
 
 let counter = 1;
-const size = slideshowImages;
+const size = slideshowImages[0].clientWidth;
+
+slideshowSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+next.addEventListener('click', () => {
+  slideshowSlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  slideshowSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prev.addEventListener('click', () => {
+  slideshowSlide.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  slideshowSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
