5f70d30335bdf4bbe6b8694be0276d07e5ecfff4
JuiceTanZ <66227184+JuiceTanZ@users.noreply.github.com>
2020-07-02 17:11:33 -0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+	echo time();
+?>
+
+<html>
+	<head>
+		<title>test page</title>
+	</head>
+    <body>
+		<h1>Admin Panel</h1>
+		<ol>
+			</li><a href="article.php?id=1">EDIT</a></li>
+		</ol>
+	</body>
+</html>
