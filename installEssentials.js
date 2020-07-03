9f06b58003c4b15b30e2a7e84e6d117cebf15643
JuiceTanZ <66227184+JuiceTanZ@users.noreply.github.com>
2020-07-02 17:13:21 -0700

    Update installEssentials.js

--- installEssentials.js
+++ installEssentials.js
@@ -1,3 +1,12 @@
 document.write("
-  
+  <head>
+		<link rel="icon" href="icon.png">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:100&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+		<noscript>
+			<h1>Please dont turn off JavaScript!</h1>
+			<h2>It may cause some pages not to work properly.</h2>
+		</noscript>
+	</head>
 ");
