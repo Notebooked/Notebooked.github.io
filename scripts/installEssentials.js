854f29a9de16050aaa9d2b86e2a28eec4efc031c
JuiceTanZ <66227184+JuiceTanZ@users.noreply.github.com>
2020-07-07 11:42:35 -0700

    yayaaayayayay

--- scripts/installEssentials.js
+++ scripts/installEssentials.js
@@ -3,7 +3,7 @@ document.write("
 		<link rel="icon" href="icon.png">
 		<link href="https://fonts.googleapis.com/css?family=Roboto:100&display=swap" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" />
 		<noscript>
 			<h1>Please dont turn off JavaScript!</h1>
 			<h2>It may cause some pages not to work properly.</h2>
