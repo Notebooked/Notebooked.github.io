f8a342075fd6687712132912d7f1ada22c63bf52
JuiceTanZ <66227184+JuiceTanZ@users.noreply.github.com>
2020-07-07 12:04:46 -0700

    e

--- scripts/installEssentials.js
+++ scripts/installEssentials.js
@@ -1,20 +1,22 @@
 document.write("
-  <head>
-		<link rel="icon" href="icon.png">
-    // Title font
-		<link href="https://fonts.googleapis.com/css?family=Roboto:100&display=swap" rel="stylesheet">
-    // Display font
-		<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-    // Serif font
-    <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">
-    // jQuery
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" />
-	</head>
-  <body>
-    <footer>
-      <p>The YouthDev name, logo, product names and logos, are trademarks of the company.</p>
-      <p id="footer-text">©" + new Date().getFullYear();"</p>
-      <a href="©" id="footer-text"></a>
-    </footer>
-  </body>
+  <!DOCTYPE html>
+    <head>
+  		<link rel="icon" href="icon.png">
+      // Title font
+  		<link href="https://fonts.googleapis.com/css?family=Roboto:100&display=swap" rel="stylesheet">
+      // Display font
+  		<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+      // Serif font
+      <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">
+      // jQuery
+  		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" />
+  	</head>
+    <body>
+      <footer>
+        <p>The YouthDev name, logo, product names and logos, are trademarks of the company.</p>
+        <p id="footer-text">©" + new Date().getFullYear();"</p>
+        <a href="©" id="footer-text"></a>
+      </footer>
+    </body>
+  </html>
 ");
