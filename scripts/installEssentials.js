241f90bc50ca61009ff42c56b1fdff0689eaf426
JuiceTanZ <66227184+JuiceTanZ@users.noreply.github.com>
2020-07-07 17:28:15 -0700

    Fixing some stuff

--- scripts/installEssentials.js
+++ scripts/installEssentials.js
@@ -1,16 +1,13 @@
-document.write("
-  <!DOCTYPE html>
-    <head>
-  		<link rel="icon" href="icon.png">
-      // Title font
-  		<link href="https://fonts.googleapis.com/css?family=Roboto:100&display=swap" rel="stylesheet">
-      // Display font
-  		<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-      // Serif font
-      <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">
-      // jQuery
-  		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" />
-  	</head>
+document.write("<link rel="icon" href="icon.png">
+// Title font
+<link href="https://fonts.googleapis.com/css?family=Roboto:100&display=swap" rel="stylesheet">
+// Display font
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+// Serif font
+<link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">
+// jQuery
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" />
+");
     <body>
       <footer>
         <p>The YouthDev name, logo, product names and logos, are trademarks of the company.</p>
@@ -19,4 +16,3 @@ document.write("
       </footer>
     </body>
   </html>
-");
